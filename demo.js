2b1fcd38af7fce90f2ec00a3cf89d50f1fd5188b
josephyhcho <josephyhcho@gmail.com>
2024-03-05 20:36:14 +0900

    first commit

---
+++ demo.js
@@ -0,0 +1,39 @@
+console.log("kkkk")
+//데이터 타입
+//string "가나다"
+//number 1 10
+1;
+
+//boolean
+true;
+false;
+
+//null
+null;
+
+//undefined
+undefined;
+
+키:180
+몸무게:70
+피부색:노란
+기독교:맞다
+박사학위:없다
+
+
+
+
+//symbol
+
+
+//변수
+height=180;
+christ=true;
+
+console.log(height);
+
+//함수
+//조건문
+//반복문
+
+"가나"
